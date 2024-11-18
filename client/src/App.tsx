59afce07196292a00dffe9f78a87713b42497290
Harit tilavat <harittilavat10@gmail.com>
2024-11-18 15:32:18 +0530

    Added folder structured + setup routing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
-
+// import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
   );
 }
 
