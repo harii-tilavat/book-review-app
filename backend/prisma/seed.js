d5616cc73afd2c941cada8ab9c5d1cadd8dbb3a2
Harit tilavat <harittilavat10@gmail.com>
2024-11-27 14:58:37 +0530

    Added initial commands for setup.

---
+++ backend/prisma/seed.js
@@ -0,0 +1,39 @@
+const prisma = require("../src/config/prismaClient");
+async function main() {
+    // Insert Genres
+    const genres = [
+        'Fiction',
+        'Non-fiction',
+        'Mystery',
+        'Fantasy',
+        'Romance',
+        'Science Fiction',
+        'Thriller',
+        'Historical',
+        'Biography',
+        'Adventure',
+    ];
+
+    await prisma.genre.deleteMany();
+    const createGenres = async () => {
+        for (let name of genres) {
+            const existingGenre = await prisma.genre.findUnique({ where: { name } });
+            if (!existingGenre) {
+                await prisma.genre.create({
+                    data: { name }
+                });
+            }
+        }
+    }
+    await createGenres();
+    console.log("Genres created... ");
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.log(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
